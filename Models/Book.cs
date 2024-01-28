4259eb879063e63d7ddc8ff9d82a1bd5a71164db
skhonosky <skhonosky@coastal.edu>
2024-01-28 14:38:48 -0500

    Finished WebAPI

--- Models/Book.cs
+++ Models/Book.cs
@@ -2,8 +2,12 @@ namespace BookApi.Models
 {
     public class Book {
         public string Title {get; set;}
-        public int ReleaseDate {get; set;}
+        public int ReleaseYear {get; set;}
         public string Author {get; set;}
         public string Genre {get; set;}
+
+        public override string ToString() {
+            return $"{Title}, {Author}, {Genre}, {ReleaseYear}";    
+        }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookApi.Repository;
+using BookApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +28,10 @@ namespace csci495bookapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddSingleton<IBookService, BookService>();
+            services.AddSingleton<IBookRepository, BookRepository>();
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
