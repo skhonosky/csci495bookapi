2b031cace93f455238b6be9d8055703d3fdf7023
skhonosky <skhonosky@coastal.edu>
2024-01-27 22:37:43 -0500

    Created WebApi foundation, began creating Book class

---
+++ Models/Book.cs
@@ -0,0 +1,9 @@
+namespace BookApi.Models
+{
+    public class Book {
+        public string Title {get; set;}
+        public int ReleaseDate {get; set;}
+        public string Author {get; set;}
+        public string Genre {get; set;}
+    }
+}
